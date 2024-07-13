f5b07dd1f702141fd26896fa8f7bac06a8650af8
muhammadhamzaaamir960 <137796896+muhammadhamzaaamir960@users.noreply.github.com>
2024-07-13 23:38:12 +0500

    modeule exports practice

---
+++ 02_moduleExport/math.js
@@ -0,0 +1,23 @@
+// const sum = (a, b) => a + b;
+// const multiply = (a, b) => a * b;
+// const sub = (a, b) => a - b;
+// const division = (a, b) => a / b;
+
+// // module.exports = { sum, multiply, sub, division };
+
+// let obj = {
+//   sum: sum,
+//   mul: multiply,
+//   sub: sub,
+//   div: division,
+// };
+
+// module.exports = obj;
+
+
+// exports.sum = (a, b) => a + b;
+// module.exports.multiply = (a, b) => a * b;
+// exports.sub = (a, b) => a - b;
+// exports.division = (a, b) => a / b;
+
+
---
+++ 02_moduleExport/math2.js
@@ -0,0 +1,11 @@
+const math  = require('./math')
+
+
+
+console.log(math)
+
+// console.log(math.sum(2,5))
+// console.log(math.division(50,35))
+// console.log(math.sub(50,35)) 
+
+
