afad823ad1d0009fe7147f2b243a750ee1cbc16d
muhammadhamzaaamir960 <137796896+muhammadhamzaaamir960@users.noreply.github.com>
2024-07-28 12:29:03 +0500

    random joke card axios practice

--- 04_axios/axioss.js
+++ 04_axios/axioss.js
@@ -1,24 +1,96 @@
-let abc = document.querySelector("h1");
+// // let abc = document.querySelector("h1");
 
-let btn = document.querySelector("button");
-let rst = document.querySelector("p");
+// // let btn = document.querySelector("button");
+// // let rst = document.querySelector("p");
 
-btn.addEventListener("click", async() => {
-  let fact = await getFacts();
-  console.log(fact)
-  rst.innerHTML = fact;
-});
+// // btn.addEventListener("click", async () => {
+// //   let fact = await getFacts();
+// //   console.log(fact);
+// //   rst.innerHTML = fact;
+// // });
 
+// let url = "https://catfact.ninja/fact";
 
-let url = "https://catfact.ninja/fact";
+// // async function getFacts() {
+// //   try {
+// //     let res = await axios.get(url);
+// //     console.log(res);
+// //     return res.data.fact;
+// //   } catch (e) {
+// //     console.log(e);
+// //     return e;
+// //   }
+// // }
 
-async function getFacts() {
-  try {
-    let res = await axios.get(url);
-    console.log(res);
-    return res.data.fact;
-  } catch (e) {
-    console.log(e);
-    return e;
-  }
-}
+// // function getData() {
+// //   let res = fetch(url);
+
+// //   res
+// //     .then((res) => {
+// //       // console.log(res);
+// //       if (res.ok) {
+// //         console.log(res.status);
+// //       } else {
+// //         throw new Error("Something went wrong!");
+// //       }
+// //     })
+// //     .catch((err) => {
+// //       console.log(err);
+// //     });
+// // }
+
+// // getData();
+
+// // async function getData() {
+// //   try {
+// //     let res = await fetch(url);
+// //     if (!res.ok) {
+// //       throw new Error("Something went wrong!");
+// //     }
+
+// //     let data = await res.json();
+// //     console.log(data.fact, ' and length is ',data.length);
+// //   } catch (e) {
+// //     console.log(e);
+// //   }
+// // }
+
+// // getData();
+
+// // async function getData() {
+// //   try {
+// //     const res = await fetch(url);
+
+// //     // Check if the response status is OK (status code 200-299)
+// //     if (!res.ok) {
+// //       throw new Error(`HTTP error! Status: ${res.status}`);
+// //     }
+
+// //     const data = await res.json();
+// //     console.log(data);
+
+// //   } catch (err) {
+// //     if (err instanceof SyntaxError) {
+// //       console.log('Error parsing JSON:', err.message);
+// //     } else if (err.name === 'TypeError') {
+// //       console.log('Network error or resource not found:', err.message);
+// //     } else {
+// //       console.log('Unknown error:', err.message);
+// //     }
+// //   }
+// // }
+
+// // getData();
+
+// let abc = new Promise((res, rej) => {
+//   setTimeout(() => {
+//     res("successful");
+//   }, 3000);
+// });
+
+// const data = async () => {
+//   let $data = await abc;
+//   console.log($data);
+// };
+
+// data();
---
+++ 04_axios/joke.js
@@ -0,0 +1,30 @@
+// const axios = require('axios')
+
+let $joke = document.querySelector("p");
+let ln = document.querySelector(".length");
+let btnn = document.querySelector(".btn");
+
+btnn.addEventListener("click", async () => {
+  let joke = await getJoke();
+  $joke.innerHTML = `Joke: ${joke.fact}`;
+  ln.innerHTML = `Length: ${joke.length}`;
+//   console.log(joke)
+});
+
+let url = "https://catfact.ninja/fact";
+
+async function getJoke() {
+  try {
+    let res = await axios.get(url);
+    if (res.data.code) {
+      throw new Error(`${res.data.code}! not found`);
+    }
+    let data = res.data;
+    return data;
+  } catch (error) {
+    console.log(
+      `Error: ${error.response.status}! ${error.response.data.message}`
+    );
+    return `Error: ${error.response.status}! ${error.response.data}`;
+  }
+}
