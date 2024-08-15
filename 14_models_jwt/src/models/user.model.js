bd6c26a649153caa5036c8fdbf8c44981eec3b42
muhammadhamzaaamir960 <137796896+muhammadhamzaaamir960@users.noreply.github.com>
2024-08-15 21:00:57 +0500

    models updated

--- 14_models_jwt/src/models/user.model.js
+++ 14_models_jwt/src/models/user.model.js
@@ -1,4 +1,6 @@
 import { model, Schema } from "mongoose";
+import bcrypt from "bcrypt";
+import jwt from "jsonwebtoken";
 
 const userSchema = new Schema(
   {
@@ -39,5 +41,15 @@ const userSchema = new Schema(
   },
   { timestamps: true },
 );
+
+userSchema.pre("save", async function (next) {
+  if (!this.isModified("password")) return next();
+  this.password = await bcrypt.hash(this.password, 10);
+  next();
+});
+
+userSchema.methods.isPasswordCorrect = async function (password) {
+  return await bcrypt.compare(password, this.password);
+};
 
 export const User = model("User", userSchema);
