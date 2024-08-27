60f6cacebc215c1e82d95c99dc0f10a493197024
muhammadhamzaaamir960 <137796896+muhammadhamzaaamir960@users.noreply.github.com>
2024-08-27 23:28:19 +0500

    controllers and routes updated

--- 16_router_and_controllers/src/app.js
+++ 16_router_and_controllers/src/app.js
@@ -1,9 +1,9 @@
 import express from "express";
 import cookieParser from "cookie-parser";
 import cors from "cors";
+import userRouter from "./routes/user.route.js";
 
 export const app = express();
-
 
 app.use(
   cors({
@@ -15,5 +15,4 @@ app.use(express.json({ limit: "16kb" }));
 app.use(express.urlencoded({ extended: true, limit: "16kb" }));
 app.use(cookieParser());
 
-
-
+app.use("/api/v1/users", userRouter);
