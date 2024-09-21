753afca528208aa2d1a0ef2dc100dfbf20a16b6c
Muhammad Hamza Aamir <mhamzaaamir960%gmail.com>
2024-09-21 20:51:24 +0500

    add routes

--- 18_user_login_logout/src/controllers/user.controller.js
+++ 18_user_login_logout/src/controllers/user.controller.js
@@ -4,6 +4,7 @@ import { User } from "../models/user.model.js";
 import { uploadOnCloudinary } from "../utils/cloudinary.js";
 import { ApiResponse } from "../utils/ApiResponse.js";
 import jwt from "jsonwebtoken";
+import mongoose, { Mongoose } from "mongoose";
 
 const generateAccessAndRefreshTokens = async (userId) => {
   try {
@@ -360,7 +361,7 @@ const updateUserCoverImage = asyncHandler(async (req, res) => {
 
 // get user channel profile controller
 const getUserProfile = asyncHandler(async (req, res) => {
-  const { username } = req.body;
+  const { username } = req.params;
 
   if (!username?.trim()) {
     throw new ApiError(400, "Username is missing");
@@ -431,6 +432,61 @@ const getUserProfile = asyncHandler(async (req, res) => {
     .json(new ApiResponse(200, channel, "Channel fetched successfully!"));
 });
 
+// get user history controller
+const getWatchHistory = asyncHandler(async (req, res) => {
+  const user = await User.aggregate([
+    {
+      $match: {
+        _id: new mongoose.Types.ObjectId(req.user._id),
+      },
+    },
+    {
+      $lookup: {
+        from: "videos",
+        localField: "watchHistory",
+        foreignField: "_id",
+        as: "watchHistory",
+        pipeline: [
+          {
+            $lookup: {
+              from: "users",
+              localField: "owner",
+              foreignField: "_id",
+              as: "owner",
+              pipeline: [
+                {
+                  $project: {
+                    fullName: 1,
+                    username: 1,
+                    avatar: 1,
+                  },
+                },
+              ],
+            },
+          },
+          {
+            $addFields: {
+              owner: {
+                $first: "$owner",
+              },
+            },
+          },
+        ],
+      },
+    },
+  ]);
+
+  return res
+    .status(200)
+    .json(
+      new ApiResponse(
+        200,
+        user[0].watchHistory,
+        "watch history fetched successfuly!",
+      ),
+    );
+});
+
 export {
   userRegister,
   userLogIn,
@@ -441,5 +497,6 @@ export {
   updateAccountDetails,
   udpateUserAvatar,
   updateUserCoverImage,
-  getUserProfile
+  getUserProfile,
+  getWatchHistory,
 };
--- 18_user_login_logout/src/routes/user.routes.js
+++ 18_user_login_logout/src/routes/user.routes.js
@@ -1,8 +1,14 @@
 import { Router } from "express";
 import { upload } from "../middlewares/multer.middleware.js";
 import {
+  changePassword,
+  getCurrentUser,
   getUserProfile,
+  getWatchHistory,
   refreshAccessToken,
+  udpateUserAvatar,
+  updateAccountDetails,
+  updateUserCoverImage,
   userLogIn,
   userLogOut,
   userRegister,
@@ -24,15 +30,21 @@ router.route("/register").post(
   ]),
   userRegister,
 );
-
 router.route("/login").post(userLogIn);
 
 // secured routes
 router.route("/logout").post(verifyJwt, userLogOut);
 router.route("/refresh-token").post(refreshAccessToken);
-router.route("/getuserprofile").get(getUserProfile)
-
-
-
+router.route("/change-passsword").patch(verifyJwt, changePassword);
+router.route("/current-user").get(verifyJwt, getCurrentUser);
+router.route("/update-account-details").patch(verifyJwt, updateAccountDetails);
+router
+  .route("/update-avatar")
+  .patch(verifyJwt, upload.single("avatar"), udpateUserAvatar);
+router
+  .route("/update-cover-image")
+  .patch(verifyJwt, upload.single("coverImage"), updateUserCoverImage);
+router.route("/c/:username").get(verifyJwt, getUserProfile);
+router.route("/watch-history").get(verifyJwt, getWatchHistory);
 
 export default router;
